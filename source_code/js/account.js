f0275b288b464f45135f727f78c81b4e74b5fc03
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-05-11 15:37:41 +0200

    HTML File Loading Added

--- source_code/js/account.js
+++ source_code/js/account.js
@@ -1,17 +1,16 @@
-const V_AllowRedirection = false;
-const V_HaveAccount = false;
-
 function F_RedirectTo(V_Link) {
     window.location.replace(V_Link);
 }
 function F_Login() {
-    F_RedirectTo('http://www.w3schools.com');
+    F_RedirectTo('http://www.w3schools.com'); // Temporary Link, Create Login Page First (TODO)
 }
 function F_SignIn() {
-    F_RedirectTo('http://www.google.com');
+    F_RedirectTo('http://www.google.com'); // Temporary Link, Create SignIn Page First (TODO)
 }
 
-if (V_AllowRedirection) {
+function F_CheckForAccount() {
+    var V_HaveAccount = false; // Add A Real Check Much More Later (TODO)
+
     if (V_HaveAccount) { F_Login(); }
     else { F_SignIn(); }
 }
---
+++ source_code/js/load_html.js
@@ -0,0 +1,7 @@
+function F_LoadHTML(V_HTMLFilePath, V_ElementId) {
+    console.log(`Loading HTML File '${V_HTMLFilePath}' for element with Id '${V_ElementId}' ...`);
+    
+    fetch(V_HTMLFilePath)
+        .then(response => response.text())
+        .then(text => document.getElementById(V_ElementId).innerHTML += text);
+}
