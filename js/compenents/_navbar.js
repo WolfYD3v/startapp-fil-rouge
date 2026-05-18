a69deb0dcad9186df31a7d41344a2337884de2cc
WolfY_D3v <181145610+WolfYD3v@users.noreply.github.com>
2026-05-18 10:24:12 +0200

    Navbar Made

--- js/compenents/_navbar.js
+++ js/compenents/_navbar.js
@@ -1,16 +1,22 @@
-console.log("d");
+console.log("Navbar - Script Loaded");
 
-const C_BurgerMenuPopupButton = document.getElementById("navbar-burger-menu-popup-button");
-const C_BurgerMenu = document.getElementById("navbar-burger-menu");
+const V_BurgerMenuPopupButton = null
+const V_BurgerMenu = null
 
-C_BurgerMenuPopupButton.addEventListener("click", function(e) {
-  console.log(this.id);
-  console.log(e.currentTarget === this);
+function F_InitVariables() {
+  V_BurgerMenuPopupButton = document.getElementById("navbar-burger-menu-popup-button");
+  V_BurgerMenu = document.getElementById("navbar-burger-menu");
+}
 
-  if (C_BurgerMenu.style.display == "none") {
-    C_BurgerMenu.style.display = "flex";
+V_BurgerMenuPopupButton.addEventListener("click", function(e) {
+  console.log("Navbar - Initializing Variables...");
+  F_InitVariables();
+
+  console.log("Navbar - Changing Burger Menu Display...");
+  if (V_BurgerMenu.style.display == "none") {
+    V_BurgerMenu.style.display = "flex";
   }
   else {
-    C_BurgerMenu.style.display = "none";
+    V_BurgerMenu.style.display = "none";
   }
 });
--- js/load_html.js
+++ js/load_html.js
@@ -1,5 +1,5 @@
 function F_LoadHTML(V_HTMLFilePath, V_ElementId) {
-    console.log(`Loading HTML File '${V_HTMLFilePath}' for element with Id '${V_ElementId}' ...`);
+    console.log(`HTML Loader - Loading HTML File '${V_HTMLFilePath}' For Element with Id '${V_ElementId}' ...`);
 
     fetch(V_HTMLFilePath)
         .then(response => response.text())
